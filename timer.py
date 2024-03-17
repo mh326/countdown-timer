f4969b7308a29a398e19ee3f7aaf51f97387cd92
mh326 <m.hiroki326@gmail.com>
2024-03-17 14:09:56 +0900

    refactor around set_seconds

--- timer.py
+++ timer.py
@@ -8,7 +8,6 @@ import flet as ft
 class CountdownTimer(ft.UserControl):
     def __init__(self, seconds, interval=0.1):
         super().__init__()
-        self._left_seconds = seconds
         self._last_time = None
         self._is_running = False
         self._interval = interval
@@ -17,9 +16,7 @@ class CountdownTimer(ft.UserControl):
             self.toggle()
 
         self.countdown = ft.TextSpan("", on_click=on_click_timer)
-        seconds = math.floor(self._left_seconds)
-        mins, secs = divmod(seconds, 60)
-        self.countdown.text = "{:02d}:{:02d}".format(mins, secs)
+        self.set_seconds(seconds, run_update=False)
 
     def will_unmount(self):
         self._is_running = False
@@ -28,23 +25,25 @@ class CountdownTimer(ft.UserControl):
         while self._left_seconds > 0 and self._is_running:
             current_time = time.time()
             elapsed_time = current_time - self._last_time
-            self._left_seconds = max(self._left_seconds - elapsed_time, 0)
+            left_seconds = max(self._left_seconds - elapsed_time, 0)
+            self.set_seconds(left_seconds)
             self._last_time = current_time
-            self.update_text()
             time.sleep(self._interval)
 
-    def set_seconds(self, seconds):
+    def set_seconds(self, seconds, run_update=True):
         self._left_seconds = seconds
-        self.update_text()
+        self.update_text(run_update)
 
-    def update_text(self):
+    def update_text(self, run_update=True):
         if self._left_seconds > 0:
             seconds = math.floor(self._left_seconds)
             mins, secs = divmod(seconds, 60)
             self.countdown.text = "{:02d}:{:02d}".format(mins, secs)
         else:
             self.countdown.text = "Time Over"
-        self.update()
+
+        if run_update:
+            self.update()
 
     def start(self):
         if self._is_running:
